f0d3444064c558d09702a4120fd51154e3b1e293
K1 <lukaszszczurek02@gmail.com>
2023-03-29 17:31:22 +0200

    test: fetch command

--- nkrsiSystem/urls.py
+++ nkrsiSystem/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from nkrsiSystem import settings
-
+adsasddasdsd
 urlpatterns = [
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
